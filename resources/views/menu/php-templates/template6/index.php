439b5532f7d6e88e2d94b26a8877bb57baaf8fbb
carter46 <mr.carter.tech07@gmail.com>
2026-06-21 00:54:45 +0100

    Update index.php

--- resources/views/menu/php-templates/template6/index.php
+++ resources/views/menu/php-templates/template6/index.php
@@ -8,7 +8,12 @@ $menuViewLevel = $menuViewLevel ?? 'home';
 $GLOBALS['t6_is_template_preview'] = ! empty($isTemplatePreview);
 $uploadBaseUrl = $uploadBaseUrl ?? (defined('UPLOAD_URL') ? rtrim(UPLOAD_URL, '/') : '');
 $popularItems = $popularItems ?? [];
-$t6BackUrl = (($menuViewLevel === 'category') && ! empty($sectionMenuUrl ?? null)) ? $sectionMenuUrl : null;
+$t6BackUrl = null;
+if ($menuViewLevel === 'section' && ! empty($fullMenuUrl)) {
+    $t6BackUrl = $fullMenuUrl;
+} elseif ($menuViewLevel === 'category' && ! empty($sectionMenuUrl)) {
+    $t6BackUrl = $sectionMenuUrl;
+}
 
 $pageTitle = $restaurant['name'] ?? 'Menu';
 if ($menuViewLevel === 'section' && ! empty($activeSection['name'])) {
